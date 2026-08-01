74917868d32549f63b5d40a529cddb5cf217b8c9
oj <jp4235>
2026-08-01 13:06:13 -0400

    Add New Parent Profile Popup

--- Home.js
+++ Home.js
@@ -7,9 +7,24 @@ document.getElementById('reportButton').addEventListener('click', () => {
 document.getElementById('blockButton').addEventListener('click', () => {
 });
 
-// jp4235: Opens the avatar selector
-document.getElementById('profileButton').addEventListener('click', () => {
+// =======================================================
+// jp4235: Profile Popup
+// =======================================================
 
-    alert("Avatar selector coming next.");
+const profilePopup = document.getElementById('profilePopup');
+
+const profileButton = document.getElementById('profileButton');
+
+const closePopup = document.getElementById('closePopup');
+
+profileButton.addEventListener('click', () => {
+
+    profilePopup.classList.add('show');
+
+});
+
+closePopup.addEventListener('click', () => {
+
+    profilePopup.classList.remove('show');
 
 });
